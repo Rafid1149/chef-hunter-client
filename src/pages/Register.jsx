e41115bf140b6a610362f6c18b50948a8991f27f
rafid149 <rafidtahmid75@gmail.com>
2023-05-05 19:37:26 +0600

    eror resolved

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -116,7 +116,7 @@ const Register = () => {
                     name="name"
                     id="name"
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-                    placeholder="Fahim Shariar"
+                    placeholder="Abcd"
                     required
                   />
                   <label className="block mt-2 mb-2 text-sm font-medium text-gray-900">
